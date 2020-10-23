a622e8e83e9058df372c095c1c3fb622b7a9647f
deziox <danzel.serrano@gmail.com>
2020-10-23 14:02:58 -0400

    Initial Commit

---
+++ GraphicsProgramming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GraphicsProgramming
+//
+//  Created by Danzel Serrano on 10/23/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
